aeea1107360e558cc88ae2389c7d61e9a3db1194
unknown <irealgoodcf@gmail.com>
2022-03-05 18:44:48 -0500

    addingEvents

--- script/main.js
+++ script/main.js
@@ -1,13 +1,36 @@
 (() => {
 
     let theThumbnails = document.querySelectorAll("#buttonHolder img"); 
-    
     let gameBoard = document.querySelector(".puzzle-board");
+    let puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
+    let dropZone = document.querySelectorAll(".drop-zone")
 
     function changeBGImg() {
     gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
     }
+    function dragStarted(event) {
+        console.log('started dragging a piece');
+        event.dataTransfer.setData('currentItem', event.target.id);
+    }
+
+    function allowDragOver(event) {
+    event.preventDefault();
+    console.log('dragged over me');
+    }
+
+    function allowDrop(event) {
+        event.preventDefault();
+        console.log('dropped on me');
+        let droppedEl = event.dataTransfer.getData('currentItem');
+        console.log(droppedEl);
+        this.appendChild(document.querySelector(`#${droppedEl}`));
+    }
 
     theThumbnails.forEach(item => item.addEventListener("click", changeBGImg));
+    puzzlePieces.forEach(piece => piece.addEventListener("dragstart", dragStarted));
+    dropZone.forEach(zone => {
+        zone.addEventListener("dragover", allowDragOver);
+        zone.addEventListener("drop", allowDrop);
+    });
     
 })()
