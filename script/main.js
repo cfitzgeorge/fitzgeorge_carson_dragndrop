d67b167fd9c230df41e8269a08b1459e9f6d73d4
unknown <irealgoodcf@gmail.com>
2022-03-05 20:31:24 -0500

    added drag and drop functionality

--- script/main.js
+++ script/main.js
@@ -5,8 +5,13 @@
     let puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
     let dropZone = document.querySelectorAll(".drop-zone")
 
+    const puzzlePaths = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+
     function changeBGImg() {
     gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`;
+    puzzlePaths.forEach((img, index) => {
+        puzzlePieces[index].src = `images/${img + this.dataset.bgref}.jpg`;
+    });
     }
     function dragStarted(event) {
         console.log('started dragging a piece');
